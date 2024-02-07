e106334037a8cafd42fac2dd6bdf69a702775683
Mahesh Chavan <mdchavan1394@gmail.com>
2024-02-07 19:40:04 +0530

    Identity Added Into The Project

--- BulkyWebBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyWebBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using BulkyWebBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWebBook.DataAccess.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -12,6 +13,8 @@ namespace BulkyWebBook.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { CategoryId = 1, CategoryName = "Action", DisplaeyCategoryOrder = 1 },
                 new Category { CategoryId = 2, CategoryName = "Sc-fi", DisplaeyCategoryOrder = 2 },
